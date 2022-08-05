b38b2a5e6ede967142358304908544a4edfd3570
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2022-08-05 20:28:54 +0600

    learn java scriot

---
+++ index.js
@@ -0,0 +1,22 @@
+// function mileToKilometer(mile) {
+//     const kilometer = mile * 1.609;
+//     return kilometer;
+
+// }
+
+// var giveMile = 3;
+// const miles = mileToKilometer(giveMile);
+
+// console.log(miles); 
+
+
+function inciToFeet(inci) {
+    const feet = inci / 12;
+    return feet;
+
+}
+
+const giveninci = 64.5;
+const inchi = inciToFeet(giveninci);
+
+console.log(inchi);
