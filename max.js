2ac8be3769051c9fed6900fc06c508312b0b1dbd
Abdulla Al Mamun <78840782+1MAMUN@users.noreply.github.com>
2022-08-07 17:13:26 +0600

    I well learning max in js

---
+++ max.js
@@ -0,0 +1,16 @@
+const jim = 84;
+const dala = 99;
+const chinku = 100;
+
+if (jim > dala && jim > chinku) {
+    console.log("jim will get the cake");
+}
+else if (dala > jim && dala > chinku) {
+    console.log("dala will get the cake")
+}
+
+else {
+    console.log(" chinku will get the cake  ")
+}
+
+
